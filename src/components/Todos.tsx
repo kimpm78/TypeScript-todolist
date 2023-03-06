3e4c090244c87d10a5a54830f5aa58a69c886f89
kim soosang <kimsoosang12@gmail.com>
2023-03-06 22:35:06 +0900

    contextAPI

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import Todo from '../models/todo';
 import TodoItem from './TodoItem';
+import { TodosContext } from '../store/todos-context';
 import classes from './Todos.module.css';
 
 // FC: function components意味
 // bind() 실행할 함수를 미리 설정할수 있음.
-const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
-  props
-) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
+      {todosCtx.items.map((item) => (
         <TodoItem
           key={item.id}
           text={item.text}
-          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
         />
       ))}
     </ul>
